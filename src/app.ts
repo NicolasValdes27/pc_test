eb975c0588d41d7d4a34fb41c3026bf4d3d8d7b2
NicolasValdes27 <nicolasignacio.vq@gmail.com>
2023-05-21 20:14:35 -0400

    Stress testing added

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import studentRoutes from './routes/student.routes';
+const jagtester = require('jagtester');
 
 const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(jagtester(app));
+
+
 
 app.use(studentRoutes);
 
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import "reflect-metadata";
 import app from './app';
 import { AppDataSource } from './db';
+require('dotenv').config()
 
 async function main() {
     try {
         await AppDataSource.initialize();
-        console.log('Database connected');
-        app.listen(3000)
-        console.log('Server in', 3000);
+        app.listen(process.env.PORT)
+        console.log('Server in', process.env.PORT);
     } catch (error) {
         console.log(error)
     }
